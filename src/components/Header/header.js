414c22d3d5902d7cceec5754a4a1885f62e16462
msbsoft2 <sadrabroo@gmail.com>
2023-02-01 21:14:06 +0330

    add log well you

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,6 +2,8 @@ import React from "react";
 import classes from './header.module.css';
 
 const Header = ({ children }) => {
+  console.log("Well, you check here, it shows that you are a professional like me and looking for bugs");
+
   return <div className={classes.container}>{children}</div>;
 };
 
